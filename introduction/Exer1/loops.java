cb378966acfa2740621452e1450af2a0a36d4f73
LukeMast <lucasamcba20@gmail.com>
2026-08-17 12:40:11 -0400

    Ainda fazendo exercícios

--- introduction/Exer1/loops.java
+++ introduction/Exer1/loops.java
@@ -14,7 +14,7 @@ public class loops {
             }
         }
         */
-
+        /* 
         //Exercício 2 - 1067
         System.out.println("Digite um valor de 1 até 1.000: ");
         int X = teclado.nextInt();
@@ -28,6 +28,25 @@ public class loops {
                     }
                 }
         }
+        */
+
+        //1078
+        //Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+        //1 x N = N      2 x N = 2N        ...       10 x N = 10N
+
+//A entrada contém um valor inteiro N (2 < N < 1000).
+
+//Imprima a tabuada de N, conforme o exemplo fornecido.
+
+        System.out.println("Digite um valor inteiro maior que 2 e menor do que 1000 para fazermos a tabuada do 1 até 10 desse respectivo valor: ");
+        int n = teclado.nextInt();
+
+        if ((n < 2) || (n > 1000)) {
+            System.out.println("Valor inválido, tente novamente");
+        } else {
+            for(int tab = 1; )
+        }
+
 
         teclado.close();
 
