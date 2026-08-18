81ae0548f78cc50d09763c742deb9e136f9629db
LukeMast <lucasamcba20@gmail.com>
2026-08-18 09:37:46 -0400

    pratic

---
+++ introduction/Exer1/GERAL.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class GERAL{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        analisartexto("Java é muito legal 123!");
+    }
+
+    public static void analisartexto(String texto) {
+        int contagemvogais = 0;
+        int contagemconsoantes = 0;
+        
+    }
+}
