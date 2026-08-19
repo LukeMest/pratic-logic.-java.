63ed6509e3afd9d804e68eda4905e58e2992cc65
LukeMast <lucasamcba20@gmail.com>
2026-08-19 18:31:48 -0400

    pratic

--- introduction/Exer1/beecrowd.java
+++ introduction/Exer1/beecrowd.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.io.IOException;
 
 public class beecrowd {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner teclado = new Scanner(System.in);
         /* 
         System.out.println("Digite dois valores para verifica se eles sao mutaplos ou nao: ");
@@ -48,6 +49,49 @@ public class beecrowd {
                 break;
             default:
                 System.out.println("DDD nao cadastrado");
+        }
+        */
+       /* 
+        System.out.println("Defina 3 caracaterísticas do seu animal, sendo elas: vertebrado/invertebrado -> ave/mamifero/inseto/anelideo -> Carnivoro/onivoro/herbivoro/hematofogo/herbivoro");
+        String animal1 = teclado.nextLine().toLowerCase();
+        String animal2 = teclado.nextLine().toLowerCase();
+        String animal3 = teclado.nextLine().toLowerCase();
+
+        if (animal1.equals("vertebrado")) {
+
+            switch (animal2) {
+                case "ave":
+                    if (animal3.equals("carnivoro")) {
+                        System.out.println("aguia");
+                    } else {
+                        System.out.println("pomba");
+                    }
+                    break;
+                case "mamifero":
+                    if (animal3.equals("onivoro")) {
+                        System.out.println("homem");
+                    } else {
+                        System.out.println("vaca");
+                    }
+                    break;
+            }
+        } else {
+            switch (animal2){
+                case "inseto":
+                    if (animal3.equals("hematofago")) {
+                        System.out.println("pulga");
+                    } else {
+                        System.out.println("lagarta");
+                    }
+                    break;
+                case "anelideo":
+                    if (animal3.equals("hematofago")) {
+                        System.out.println("sanguessuga");
+                    } else {
+                        System.out.println("minhoca");
+                    }
+                    break;
+            }
         }
         */
         teclado.close();
